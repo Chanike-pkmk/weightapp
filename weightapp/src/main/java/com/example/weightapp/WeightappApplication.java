35262d1a2486ec9e40f1c4c97caa1d8e35d0a614
池田亘輝 <@DESKTOP-E5495QR>
2026-03-08 23:14:51 +0900

    体重管理アプリの初期プロジェクトを作成

---
+++ weightapp/src/main/java/com/example/weightapp/WeightappApplication.java
@@ -0,0 +1,13 @@
+package com.example.weightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightappApplication.class, args);
+	}
+
+}
---
+++ weightapp/src/test/java/com/example/weightapp/WeightappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weightapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
